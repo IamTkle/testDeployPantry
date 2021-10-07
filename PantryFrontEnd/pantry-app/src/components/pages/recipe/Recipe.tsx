f28af5265cd704639bf92827acc55b99cff80945
evinle <imevinle@gmail.com>
2021-10-07 17:45:45 +0800

    Added tabs to recipes page

--- PantryFrontEnd/pantry-app/src/components/pages/recipe/Recipe.tsx
+++ PantryFrontEnd/pantry-app/src/components/pages/recipe/Recipe.tsx
@@ -26,11 +26,19 @@ const useStyles = makeStyles((theme: Theme) =>
       color: theme.palette.primary.dark,
       backgroundColor: theme.palette.primary.light,
       width: "100%",
+      position: "sticky",
+      bottom: 0,
+      [theme.breakpoints.up("md")]: {},
+    },
+
+    tabIndicator: {
+      top: 0,
     },
 
     fab: {
       position: "fixed",
-      bottom: theme.spacing(2),
+      zIndex: 5,
+      bottom: theme.spacing(11),
       right: theme.spacing(2),
       color: theme.palette.background.default,
       "&:hover": {
@@ -64,25 +72,51 @@ const Recipe: React.FC<RecipeProps> = ({ setNavOpen }) => {
         handleSortTypeChosen={handleSortTypeChosen}
       />
       <Toolbar />
-      <Tabs
-        value={activeTab}
-        variant="fullWidth"
-        onChange={handleTabChange}
-        classes={{ root: classes.tabBar }}
-      >
-        <Tab wrapped label="browse" value={0} icon={<List />} />
-        <Tab wrapped label="favorites" value={1} icon={<Favorite />} />
-      </Tabs>
-      <Container>
+      <Container style={{ paddingBottom: 16 }}>
+        <RecipeEntry
+          ingredients={["bols in yo jaws"]}
+          name="My Recipe"
+          recipeID="3432P"
+        />
+
+        <RecipeEntry
+          ingredients={["bols in yo jaws"]}
+          name="My Recipe"
+          recipeID="3432P"
+        />
+        <RecipeEntry
+          ingredients={["bols in yo jaws"]}
+          name="My Recipe"
+          recipeID="3432P"
+        />
+        <RecipeEntry
+          ingredients={["bols in yo jaws"]}
+          name="My Recipe"
+          recipeID="3432P"
+        />
+        <RecipeEntry
+          ingredients={["bols in yo jaws"]}
+          name="My Recipe"
+          recipeID="3432P"
+        />
         <RecipeEntry
           ingredients={["bols in yo jaws"]}
           name="My Recipe"
           recipeID="3432P"
         />
       </Container>
-      <Fab size="large" color="secondary" className={classes.fab}>
+      <Fab size="large" color="secondary" classes={{ root: classes.fab }}>
         <Add />
       </Fab>
+      <Tabs
+        value={activeTab}
+        variant="fullWidth"
+        onChange={handleTabChange}
+        classes={{ root: classes.tabBar, indicator: classes.tabIndicator }}
+      >
+        <Tab wrapped label="browse" value={0} icon={<List />} />
+        <Tab wrapped label="favorites" value={1} icon={<Favorite />} />
+      </Tabs>
     </div>
   );
 };
